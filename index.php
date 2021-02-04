5f2d5817f87a134e6e297c7919f0155a81d78fe7
“adrianpisabarrogarcia” <adrian.pisabarro@ikasle.egibide.org>
2021-02-04 07:00:53 +0100

    asdf-

--- index.php
+++ index.php
@@ -1,16 +1,15 @@
 <?php
 /**
-* @author evilnapsis
+* @author adriantxuipisi
 **/
 function connect(){
     
 
     try{
-        //$db = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_d6be82b678b6e84",'bc2d8f1edc0fa4','fbfee04e');
-        $db = new PDO("mysql:eu-cdbr-west-03.cleardb.net;dbname=heroku_f20f4a87ca11627",'b1441ce443f229','0ec77b52');
-
+        $user="bc2d8f1edc0fa4";$pass="fbfee04e";$host="eu-cdbr-west-03.cleardb.net";$ddbb="dbname=heroku_d6be82b678b6e84";
+        $con = new mysqli($host,$user,$pass,$ddbb);
         echo "conexión correcta a la base de datos";
-        return $db;
+    
     }
     catch(PDOException $e){
         echo $e->getMessage();
