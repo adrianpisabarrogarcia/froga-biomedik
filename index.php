f3bedeb403bc934c1ad8cd7ad5ee5c953206b8d0
“adrianpisabarrogarcia” <adrian.pisabarro@ikasle.egibide.org>
2021-02-04 06:55:34 +0100

    asd

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ function connect(){
     
 
     try{
-        $db = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_d6be82b678b6e84",'bc2d8f1edc0fa4','fbfee04e');
+        //$db = new PDO("mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_d6be82b678b6e84",'bc2d8f1edc0fa4','fbfee04e');
+        $db = new PDO("mysql:eu-cdbr-west-03.cleardb.net;dbname=heroku_f20f4a87ca11627",'b1441ce443f229','0ec77b52');
+
         echo "conexión correcta a la base de datos";
         return $db;
     }
